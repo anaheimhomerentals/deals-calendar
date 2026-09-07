52ccbd703c733e5dcd3d5579f9827c1db4443483
Jen <anaheimhomerentals@gmail.com>
2026-09-06 20:00:45 -0700

    Automated hourly backup Sun 09/06/2026 20:00:45.39

--- autopush.bat
+++ autopush.bat
@@ -1,5 +1,11 @@
 @echo off
 cd /d "C:\Users\wkstnlocal\Desktop\deals calendar"
-git add .
-git commit -m "Automated hourly backup %date% %time%"
-git push origin main
+
+:: Check if there are any modified or untracked files
+for /f "tokens=*" %%a in ('git status --porcelain') do set "HAS_CHANGES=1"
+
+if defined HAS_CHANGES (
+    git add .
+    git commit -m "Automated hourly backup %date% %time%"
+    git push origin main
+)
